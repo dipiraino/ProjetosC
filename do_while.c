9fe8d2569924955113fca1b8f972c1f3cbf6bcac
dipiraino <suportedipiraino@gmail.com>
2026-07-30 19:44:51 -0300

    Estudos Do -  while - for - else

---
+++ do_while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+    int i=0;
+
+
+    do{
+        printf("%d ", i);
+        sleep(1);
+        printf("%d ", i);
+        i++;
+    }
+    while (i <= 10);
+    
+    
+}
